74981f418d5952585be7d00f265586e994c03b94
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-08-18 17:08:45 +0500

    Practising

--- sw.js
+++ sw.js
@@ -1,3 +1,24 @@
+let cacheName = "pwa";
+
+let filesToCache = ["/", "/index.html", "/app.js"];
+
 self.addEventListener("activate", () => {
   console.log("Service Worker Activated");
+});
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log("caching");
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
